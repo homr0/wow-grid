06fbbd1039bc84fa68dab7ae069c41fb796a6e16
Rebecca Hom <rhom001@ucr.edu>
2018-01-30 12:20:21 -0800

    added in editor class

--- src/js/wow-editor/wow-exit.js
+++ src/js/wow-editor/wow-exit.js
@@ -1,16 +1,17 @@
 // Functionality for saving and exiting as well as saving without exiting the Wow Grid Editor.
 function wowClean() {
     // Attaches all strings to be deleted.
-    $('.wow-strings').append(control.section, control.row, control.column, control.module).remove();
+    $('.wow-strings').append(hover.section, hover.row, hover.column, hover.module).remove();
+
+    // Removes the editor indicator
+    $('.wow-editor').removeClass('wow-editor');
 }
 
 $(wow.saveExit).on('click', function() {
-    console.log("Clicked save button");
     wowClean();
 });
 
 $(wow.noSave).on('click', function() {
-    console.log("Clicked to exit without saving");
     wowClean();
     wowReturn();
 });
@@ -18,10 +19,8 @@ $(wow.noSave).on('click', function() {
 function wowReturn() {
     setTimeout(function() {
         if($(wow.editor).length == 0) {
-            console.log("Going to go wait");
             wowWait();
         } else {
-            console.log("Need to return");
             wowReturn();
         }
     }, 150);
---
+++ src/js/wow-editor/wow-init.js
@@ -0,0 +1,6 @@
+// Sets up the code for editing.
+// Adds in the editor class.
+$(wow.editor + " " + wow.section + ", "
+    + wow.editor + " " + wow.row + ", "
+    + wow.editor + " " + wow.column + ", "
+    + wow.editor + " " + wow.module).addClass('wow-editor');
