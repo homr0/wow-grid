25d0e1fe1e2b2f6284f84d79d1ff20057470a1bf
Rebecca Hom <rebecca.hom001@gmail.com>
2018-02-09 17:11:07 -0800

    added in function to determine if up and/or down arrow needs to be shown

--- src/js/wow-editor/wow-content.js
+++ src/js/wow-editor/wow-content.js
@@ -14,7 +14,7 @@ $.fn.extend({
 
 // Starts the Wow Grid Editor
 function wowGrid(preview) {
-    // Button functions/
+    // Button functions.
     // Contextual/hover menu functionality for all components of the grid.
     $('body').on({
         mouseenter: function() {
@@ -28,6 +28,35 @@ function wowGrid(preview) {
     $(preview).after('<div id="wow-modal-menu" class="mfp-hide"></div>');
 } wowGrid(wow.editor);
 
+// Handles setting up the hover menu.
 function hoverMenu() {
+    // Detaches any menus that are out.
+    $('.wow-menu').detach();
 
+    // Helper functions
+    // function sortUpDown(): Moving arrows are shown or hidden depending on number of sections/rows and position of current section or row. If there is only one section or row, then no arrows are shown. If the section or row is at the top or bottom of its container, then only the bottom or top arrow is shown respectively.
+    function sortUpDown() {
+        $('.wow-menu').removeClass('wow-endpoint wow-single').children('.wow-up, .wow-down').show();
+
+        // Figures out the number of siblings to the current component.
+        var siblings = $('.wow-hover').parent();
+        if($('.wow-hover').hasClass(wow.section)) {
+            $(siblings).children(wow.section);
+        } else if($('.wow-hover').hasClass(wow.row)) {
+            $(siblings).children(wow.row);
+        }
+
+        // Shows the proper arrows.
+        if($(siblings).length == 1) {
+            $('wow-menu').addClass('wow-single');
+        } else if($(siblings).first().hasClass('wow-hover')) {
+            $('wow-menu').addClass('wow-endpoint wow-end-top');
+        } else if($(siblings).last().hasClass('wow-hover')) {
+            $('wow-menu').addClass('wow-endpoint wow-end-bottom');
+        }
+    }
+
+    function sortDrag() {
+        
+    }
 }
