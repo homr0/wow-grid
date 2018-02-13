a99bc2d3428a9809ff08a32659057e0903e3c0c3
Rebecca Hom <rebecca.hom001@gmail.com>
2018-02-12 16:32:16 -0800

    added in containers for sortables and editable content area

--- src/js/wow-editor/wow-exit.js
+++ src/js/wow-editor/wow-exit.js
@@ -1,10 +1,41 @@
 // Functionality for saving and exiting as well as saving without exiting the Wow Grid Editor.
 function wowClean() {
+    // Removes the sortables and other event handlers.
+    sortable('.wow-editor' + wow.row, 'destroy');
+    sortable('.content-container', 'destroy');
+    $('.wow-editor').removeAttr('data-item-sortable-id');
+    $('body').off('mouseenter', '**');
+    $('body').off('click', '**');
+    $('body').off('mouseleave', '**');
+
     // Attaches all strings to be deleted.
     $('.wow-strings').append(hover.section, hover.row, hover.column, hover.module).remove();
 
-    // Removes the editor indicator
+    // Cleans up any empty class, style, and id attributes.
+    $('*[class=""]').removeAttr('class');
+    $('.wow-editor').each(function() {
+        if(($(this).attr('id') === "") || ($(this).attr('id') === undefined) || ($(this).attr('id') === null)) {
+            $(this).removeAttr('id');
+        }
+    });
+    $('*[style=""]').removeAttr('style');
+
+    // Removes the editor indicators.
     $('.wow-editor').removeClass('wow-editor');
+    $('.wow-hover').removeClass('wow-hover');
+    $('.wow-highlight').removeClass('wow-highlight');
+
+    // Saves module content.
+    $(wow.editor + " " + wow.column).each(function(index) {
+        $(this).find(wow.module).each(function() {
+            var wowContent = $(this).find('.editable-bounds').contents();
+            $(this).find('.editable-bounds').replaceWith(wowContent);
+        });
+
+        var modules = $(this).find(wow.module).detach();
+        $(this).find('.content-container').remove();
+        $(this).append(modules);
+    });
 }
 
 $(wow.saveExit).on('click', function() {
--- src/js/wow-editor/wow-init.js
+++ src/js/wow-editor/wow-init.js
@@ -4,3 +4,16 @@ $(wow.editor + " " + wow.section + ", "
     + wow.editor + " " + wow.row + ", "
     + wow.editor + " " + wow.column + ", "
     + wow.editor + " " + wow.module).addClass('wow-editor');
+
+// Sets up the modules by adding in content containers and editable bounds.
+$(wow.editor + " "  + wow.column).each(function() {
+    $(this).append('<div class="content-container"></div>');
+    console.log($(this).html());
+
+    var modules = $(this).children(wow.module).detach();
+    console.log($(modules).html());
+    $(this).children('.content-container').append(modules);
+    $(this).find(wow.module).each(function() {
+        $(this).wrapInner('<div class="editable-bounds" contenteditable="true"></div>');
+    });
+});
