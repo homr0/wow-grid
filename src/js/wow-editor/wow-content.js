cbc2454d1bffe581027f085be2180b1cd1f2ecbf
Rebecca Hom <rebecca.hom001@gmail.com>
2018-02-12 17:10:57 -0800

    added in button functionality for hover components

--- src/js/wow-editor/wow-content.js
+++ src/js/wow-editor/wow-content.js
@@ -20,15 +20,54 @@ function wowGrid(preview) {
         mouseenter: function() {
             $('.wow-hover').removeClass('wow-hover');
             $(this).addClass('wow-hover');
-            hoverMenu();
+            wowMenu();
+
+            // Configures a component.
+            $('.wow-edit').off().on('click', function() {
+                focused = $(this).parent().parent();
+                wowEdit($(focused));
+            });
+
+            // Duplicates a component.
+            $('.wow-duplicate').off().on('click', function() {
+                focused = $(this).parent().parent();
+                wowDuplicate($(focused));
+            });
+
+
+        },
+        click: function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $('.wow-editor.wow-highlight').removeClass('wow-highlight');
+            $(this).addClass('wow-highlight');
+        },
+        mouseleave: function() {
+            // Since modules are not direct children of columns, the parent for the modules need to move to the next level parent to reach the column.
+            var parent = $(this).parent();
+            if($(this).hasClass(wow.module.slice(1))) {
+                parent = $(parent).parent();
+            }
+
+            // Puts the menu on to the parent component due to hovering issue.
+            $('.wow-hover').removeClass('wow-hover');
+            $(parent).addClass('wow-hover');
+            wowMenu();
         }
     }, '.wow-editor');
+
+    // Removes any Wow Editor components when not in a valid area.
+    $('body').on('click', ':not(.wow-editor)', function() {
+        $('.wow-editor.wow-highlight').removeClass('wow-highlight');
+        $('.wow-hover').removeClass('wow-hover');
+        $('.wow-menu, .wow-add').remove();
+    });
 
     $(preview).after('<div id="wow-modal-menu" class="mfp-hide"></div>');
 } wowGrid(wow.editor);
 
-// Handles setting up the hover menu.
-function hoverMenu() {
+// Sets up the hover menu.
+function wowMenu() {
     // Cleans and detaches any menus that are out.
     $('.wow-menu, .wow-add').removeClass('wow-end-top wow-end-bottom wow-single').detach();
     var component = "";
@@ -94,4 +133,28 @@ function hoverMenu() {
             $('.wow-sort-ghost').removeClass('wow-sort-ghost');
         });
     }
+}
+
+// Configures the component.
+function wowEdit() {
+
+}
+
+
+// Duplicates the component.
+function wowDuplicate(wowFocus) {
+    $(wowFocus).children('.wow-menu').removeClass('wow-single');
+    $(wowFocus).clone().insertAfter($(wowFocus)).animateCss('zoomIn');
+
+    var wowCloned = $(wowFocus).next();
+    $(wowCloned).removeClass('wow-highlight');
+
+    if($(wowCloned).attr('id') !== "") {
+
+    }
+}
+
+// Checks to make sure that the component name is valid.
+function wowCheckName(name) {
+    var uniqueID = true;
 }
